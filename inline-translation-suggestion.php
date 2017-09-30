fee0e7574f3a5280702a658b7ccc04aaad1f5016
Swapnil V. Patil <patilswapnilv@gmail.com>
2017-09-30 17:39:08 +0530

    initial commit

---
+++ inline-translation-suggestion.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Inline Translation Suggestion
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://swapnil.blog/
+ * @since             1.0.0
+ * @package           inline-translation-suggestion
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Inline Translation Suggestion
+ * Plugin URI:        https://github.com/wpindiaorg/inline-translation-suggestion
+ * Description:       WordPress plugin to help contribute for the WordPress Translations
+ * Version:           1.0.0
+ * Author:            patilswapnilv
+ * Author URI:        https://swapnil.blog/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       inline-translation-suggestion
+ * Domain Path:       /languages
+ */
+
+
+
